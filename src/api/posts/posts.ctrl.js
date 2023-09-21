a6f8d322d8487880ed6e015bcd971c1137790080
MarcoChanwoo <cwhong8087@gmail.com>
2023-09-21 17:03:01 +0900

    update

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -34,6 +34,59 @@ export const list = async (ctx) => {
         ctx.throw(500, e);
     }
 };
-export const read = (ctx) => {};
-export const remove = (ctx) => {};
-export const update = (ctx) => {};
+
+/*
+    GET /api/posts/:id
+*/
+export const read = async (ctx) => {
+    const { id } = ctx.params;
+    try {
+        const post = await Post.findById(id).exec();
+        if (!post) {
+            ctx.status = 404; // Not Found
+            return;
+        }
+        ctx.body = post;
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+};
+
+/*
+    DELETE /api/posts/:id
+*/
+export const remove = async (ctx) => {
+    const { id } = ctx.params;
+    try {
+        await Post.findByIdAndRemove(id).exec();
+        ctx.status = 204; // No Content(성공했으나 응답할 데이터가 없음)
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+};
+
+/*
+    PATCH /api/posts/:id
+    {
+        title: 수정,
+        body: 수정 내용,
+        tags: [수정, 태그]
+    }
+*/
+export const update = async (ctx) => {
+    const { id } = ctx.params;
+    try {
+        const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+            new: true,
+            // true: 업데이트된 데이터를 반환
+            // false: 업데이트되기 전의 데이터를 반환
+        }).exec();
+        if (!post) {
+            ctx.status = 404;
+            return;
+        }
+        ctx.body = post;
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+};
