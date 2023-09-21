c2e0537f51ecb7dfcfde5cff2b2f975720c3952b
MarcoChanwoo <cwhong8087@gmail.com>
2023-09-21 17:59:53 +0900

    update

---
+++ src/createFakeData.js
@@ -0,0 +1,15 @@
+// 가짜 데이터 생성(페이지네이션)
+import Post from './models/post';
+
+export default function createFakeData() {
+    // 0, 1, 2, ..., 39로 이뤄진 배열을 생성 후 포스트 데이터로 변환
+    const posts = [...Array(40).keys()].map((i) => ({
+        title: `포스트 #${i}`,
+        //
+        body: 'Que es Lorem Ipsum? Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas Letraset, las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.',
+        tags: ['가짜', '데이터'],
+    }));
+    Post.insertMany(posts, (err, docs) => {
+        console.log(docs);
+    });
+}
